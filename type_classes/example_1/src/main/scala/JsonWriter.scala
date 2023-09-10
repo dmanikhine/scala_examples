36c39eb92462335843454a137b69681573a14940
Denis Manikhin <dmanikhine@gmail.com>
2023-09-10 20:42:56 +0200

    type classes step 1

---
+++ type_classes/example_1/src/main/scala/JsonWriter.scala
@@ -0,0 +1,24 @@
+trait JsonConvertible {
+    def convertToJson: JsonValue
+}
+
+object JsonWriter {
+    def write(value:JsonValue): String =
+    value match {
+        case JsonObject(entries)=>{
+            val serializedEntries=
+            for ((key,value)<- entries) yield key + ": " + write(value)
+            "{ " + (serializedEntries mkString ", ") + " }"           
+        }
+        case JsonArray(entries)=> {
+            val serializedEntries=entries map write
+            "[ " + (serializedEntries mkString ", ") + " ]"
+        }
+        case JsonString(value)=> "\""+ value + "\""
+        case JsonNumber(value)=> value.toString
+        case JsonBoolean(value) => value.toString
+        case JsonNull => "null"
+    }
+    def write(value: JsonConvertible):String =
+    write (value.convertToJson)
+}
--- type_classes/example_1/src/main/scala/Main.scala
+++ type_classes/example_1/src/main/scala/Main.scala
@@ -5,6 +5,7 @@
 
 object Main extends App { 
 
-  
+  val expr:Expression=Plus(Number(1),Minus(Number(3),Number(2)))
+  println(JsonWriter.write(expr))
     
 }
